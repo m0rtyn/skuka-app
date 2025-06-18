60b92ce6a7a0f0b963714ad99e9386d6a2469fa7
Martyn <zogacc@gmail.com>
2025-06-18 13:35:50 +0200

    chore: debug app version from env

--- src/shared/utils/show-app-version.ts
+++ src/shared/utils/show-app-version.ts
@@ -1,11 +1,10 @@
 export const showAppVersion = () => {
   console.info(
-    window,
-    window?.__VITE_VERSION__,
-    window?.__VITE_NAME__,
+    __VITE_VERSION__,
+    __VITE_NAME__,
     `
-%cVER: ${import.meta.env.VITE_VERSION} ${window?.__VITE_VERSION__}
-NAME: ${import.meta.env.VITE_NAME} ${window?.__VITE_NAME__}
+%cVER: ${import.meta.env.VITE_VERSION} ${__VITE_VERSION__}
+NAME: ${import.meta.env.VITE_NAME} ${__VITE_NAME__}
 `,
     "background: #222; color: #bada55"
   )
